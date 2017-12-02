c834b890a8104d751ad87b6201faa26b9c90ea09
tyboth <alexis.marion.8@hotmail.fr>
2017-12-02 01:24:08 +0100

    Rename main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygame_test',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='alexismarion',
+    author_email='',
+    description=''
+)
